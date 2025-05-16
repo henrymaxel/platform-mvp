0e3ed6cfbec53b21857c7382e9486dd3bce5db17
henrymaxel <henrymaxel01@gmail.com>
2025-05-16 15:36:26 -0700

    Added wallet connection
    accidentally worked on dev branch.
    
    really early access, code is a mess and need to be reconfigured completely but we can connect wallet and find tokens and metadata of given contract and console log it.

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -2,14 +2,15 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { 
-  User, 
-  CreditCard, 
-  Bell, 
-  Lock, 
-  Shield, 
+import {
+  User,
+  CreditCard,
+  Bell,
+  Lock,
+  Shield,
   ChevronRight,
-  BookOpen
+  BookOpen,
+  Wallet
 } from 'lucide-react';
 
 export default function SettingsPage() {
@@ -21,48 +22,55 @@ export default function SettingsPage() {
       description: 'Update your personal information',
       icon: User,
       href: '/dashboard/settings/profile',
-      color: 'text-blue-500',
+      color: 'text-myred-500',
     },
     {
       title: 'Subscription',
       description: 'Manage your subscription plan',
       icon: CreditCard,
       href: '/dashboard/settings/subscriptions',
-      color: 'text-green-500',
+      color: 'text-myred-500',
     },
     {
       title: 'Account Security',
       description: 'Update password and security settings',
       icon: Shield,
       href: '/dashboard/settings/security',
-      color: 'text-purple-500',
+      color: 'text-myred-500',
     },
     {
       title: 'Notification Preferences',
       description: 'Control how you receive notifications',
       icon: Bell,
       href: '/dashboard/settings/notifications',
-      color: 'text-yellow-500',
+      color: 'text-myred-500',
     },
     {
       title: 'My Publications',
       description: 'Manage your published content',
       icon: BookOpen,
       href: '/dashboard/settings/publications',
-      color: 'text-orange-500',
+      color: 'text-myred-500',
     },
+    {
+      title: 'Wallet Connect',
+      description: 'Connect your wallets to verify your asset ownership',
+      icon: Wallet,
+      href: '/dashboard/settings/wallet',
+      color: 'text-myred-500',
+    }
   ];
 
   return (
     <div className="p-4 md:p-6 h-full overflow-y-auto mt-10">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-2xl md:text-3xl font-bold mb-6">Settings</h1>
-        
+
         <div className="grid grid-cols-1 gap-4">
           {settingsOptions.map((option, index) => {
             const IconComponent = option.icon;
             return (
-              <div 
+              <div
                 key={index}
                 onClick={() => router.push(option.href)}
                 className="bg-gray-800 rounded-lg p-4 shadow-lg flex items-center cursor-pointer hover:bg-gray-700 transition-colors"
